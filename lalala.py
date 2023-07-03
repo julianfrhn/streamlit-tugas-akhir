07cad7475e6d1d6323cbbc2d1a33c13ea8a4b4f1
julianfrhn <49059292+julianfrhn@users.noreply.github.com>
2023-07-03 21:53:29 +0700

    Add files via upload

--- lalala.py
+++ lalala.py
@@ -105,4 +105,4 @@ if (selected == 'About The Application') :
 
     st.write('Tujuan Dari Aplikasi Ini')
     st.write('Di era web seperti sekarang, sejumlah informasi kini mengalir melalui jaringan. Karena berbagai konten web meliputi opini subjektif serta informasi yang objektif, sekarang umum bagi orang-orang untuk mengumpulkan informasi tentang produk dan jasa yang mereka ingin beli. Namun karena cukup banyak informasi yang ada dalam bentuk teks tanpa ada skala numerik, sulit untuk mengklasifikasikan evaluasi informasi secara efisien tanpa membaca teks secara lengkap. Maka Tujuan dari aplikasi ini adalah untuk mempermudah pengguna dalam mencari informasi mengenai Restoran Bakmie di Bandung. ')
-    st.write('© Julianfarhan, TA 2023 ')
+    st.write('© Julian Farhan Wijaya | Putu Harry Gunawan, TA 2023 ')
